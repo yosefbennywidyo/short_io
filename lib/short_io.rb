3cb8b99ba8d11c3bf083ebaecfe6525c104aad9a
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-25 13:00:21 +0800

    Start create Short IO gem

---
+++ lib/short_io.rb
@@ -0,0 +1,6 @@
+require "short_io/version"
+
+module ShortIo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/short_io_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class ShortIoTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ShortIo::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
