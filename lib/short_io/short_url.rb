06b40316b91ebb3efbb4e9221f302b0e5a465d5c
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-25 19:56:43 +0800

    Update ShortUrl lib: - add memoization - add check_variables method to raise error   when `nil` or `empty` on host name and api_key

--- lib/short_io/short_url.rb
+++ lib/short_io/short_url.rb
@@ -8,13 +8,19 @@ module ShortIo
     REQUEST_TYPE      = 'application/json'
     SHORT_IO_BASE_URL = 'https://api.short.io/domains/'
     
-    def initialize(host_name = 'example.com', api_key = 'API_KEY', options={})
-      @host_name    = host_name
-      @api_key      = api_key
-      @hide_referer = options.key?(:hide_referer)  ? options[:hide_referer] : false
-      @https_link   = options.key?(:https_link)    ? options[:https_link]   : false
-      @link_type    = options.key?(:link_type)     ? options[:link_type]    : 'random'
-      @short_io_base_url = 'https://api.short.io/domains/'
+    def initialize(host_name, api_key, options={})
+      @host_name          ||= host_name
+      @api_key            ||= api_key
+      @hide_referer       ||= options.key?(:hide_referer)  ? options[:hide_referer] : false
+      @https_link         ||= options.key?(:https_link)    ? options[:https_link]   : false
+      @link_type          ||= options.key?(:link_type)     ? options[:link_type]    : 'random'
+      @short_io_base_url  ||= SHORT_IO_BASE_URL
+      check_variables
+    end
+
+    def check_variables
+      raise ShortIo::HostNameError.new(host_name: 'Please provide a host name') if (@host_name.nil? || @host_name.empty?)
+      raise ShortIo::ApiKeyError.new(api_key: 'Please provide an API key') if (@api_key.nil? || @api_key.empty?)
     end
 
     def setup
