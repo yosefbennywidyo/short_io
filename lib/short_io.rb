fc8721d9a9d52a1da2d6a8a9e409e738b9d636eb
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-01-25 19:54:58 +0800

    Update ShortIo Error: - HostNameError - ApiKeyError

--- lib/short_io.rb
+++ lib/short_io.rb
@@ -3,5 +3,7 @@ require "short_io/short_url"
 
 module ShortIo
   class Error < StandardError; end
+  class HostNameError < Error; end
+  class ApiKeyError < Error; end
   # Your code goes here...
 end
