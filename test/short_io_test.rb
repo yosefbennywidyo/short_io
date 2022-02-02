c4b03cb953580ed50314fc2afe72f607db67c716
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-02-03 04:29:14 +0800

    Update ShortIoTest: - update test version to `0.1.8` - add setup test - move create_new_short_url to method - update default_initialize method to check instance variables

--- test/short_io_test.rb
+++ test/short_io_test.rb
@@ -8,7 +8,7 @@ class ShortIoTest < Minitest::Test
   SECOND_EXAMPLE_API_KEY  = 'API_KEY_2'
 
   def test_that_it_has_latest_version_number
-    assert_match ::ShortIo::VERSION, '0.1.7'
+    assert_match ::ShortIo::VERSION, '0.1.8'
   end
 
   def test_it_initialize_short_url_correctly
@@ -43,17 +43,32 @@ class ShortIoTest < Minitest::Test
     default_initialize(DEFAULT_HOST_NAME, DEFAULT_API_KEY, {hide_referer: true, https_link: true, link_type: 'long'})
   end
 
-  def create_new_short_url(host_name=DEFAULT_HOST_NAME, api_key=DEFAULT_API_KEY, options={})
-    ::ShortIo::ShortUrl.new(host_name, api_key, options)
+  def test_it_setup
+    create_new_short_url(DEFAULT_HOST_NAME, DEFAULT_API_KEY)
+    @short_url.setup
+    @short_url.yield_self.inspect.include? '@url'
+    @short_url.yield_self.inspect.include? SHORT_IO_BASE_URL
+    @short_url.yield_self.inspect.include? '@http'
+    @short_url.yield_self.inspect.include? 'api.short.io:443'
   end
 
-  def default_initialize(host_name=DEFAULT_HOST_NAME, api_key=DEFAULT_API_KEY, options={})
-    short_url = create_new_short_url(host_name, api_key, options)
-    assert_equal short_url.instance_variable_get('@api_key'), api_key
-    assert_equal short_url.instance_variable_get('@hide_referer'), options.key?(:hide_referer) ? options[:hide_referer] : false
-    assert_equal short_url.instance_variable_get('@host_name'), host_name
-    assert_equal short_url.instance_variable_get('@https_link'), options.key?(:https_link) ? options[:https_link] : false
-    assert_equal short_url.instance_variable_get('@link_type'), options.key?(:link_type) ? options[:link_type] : 'random'
-    assert_equal short_url.instance_variable_get('@short_io_base_url'), SHORT_IO_BASE_URL
+  def create_new_short_url(host_name=DEFAULT_HOST_NAME, api_key=DEFAULT_API_KEY, short_io_base_url=SHORT_IO_BASE_URL, options={})
+    @short_url = ::ShortIo::ShortUrl.new(host_name, api_key, options)
+  end
+
+  def default_initialize(host_name=DEFAULT_HOST_NAME, api_key=DEFAULT_API_KEY, short_io_base_url=SHORT_IO_BASE_URL, options={})
+    create_new_short_url(host_name, api_key, options)
+    @short_url.yield_self.inspect.include? '@api_key'
+    @short_url.yield_self.inspect.include? "#{api_key}"
+    @short_url.yield_self.inspect.include? '@hide_referer'
+    @short_url.yield_self.inspect.include? "#{options.key?(:hide_referer) ? options[:hide_referer] : false}"
+    @short_url.yield_self.inspect.include? '@host_name'
+    @short_url.yield_self.inspect.include? "#{host_name}"
+    @short_url.yield_self.inspect.include? '@https_link'
+    @short_url.yield_self.inspect.include? "#{options.key?(:https_link) ? options[:https_link] : false}"
+    @short_url.yield_self.inspect.include? '@link_type'
+    @short_url.yield_self.inspect.include? options.key?(:link_type) ? options[:link_type] : 'random'
+    @short_url.yield_self.inspect.include? '@short_io_base_url'
+    @short_url.yield_self.inspect.include? SHORT_IO_BASE_URL
   end
 end
